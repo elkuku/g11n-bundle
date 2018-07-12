d76b4fd17622140d88323fd51f6787d00862680f
Nikolai Plath <der.el.kuku@gmail.com>
2018-07-12 10:13:29 -0500

    Start with tests

---
+++ Tests/Twig/G11nExtensionTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: test
+ * Date: 11/07/18
+ * Time: 17:59
+ */
+
+namespace ElKuKu\G11nBundle\Tests\Twig;
+
+use ElKuKu\G11nBundle\Twig\G11nExtension;
+use PHPUnit\Framework\TestCase;
+
+class G11nExtensionTest  extends TestCase
+{
+    public function testThis()
+    {
+        $extension = new G11nExtension('.');
+
+        $this->assertNotEmpty($extension->getCurrentLang());
+    }
+}
